b925aff6dd1d2de552fd12a6fc656a03e6a96c5e
Stephen <rursteve2@yahoo.com>
2019-07-28 03:35:44 -0400

    created react app

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+
+
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Titanic</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
